9c9211bb5293b60354a463a7c173596e26d19b14
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-16 22:43:42 -0400

    Adding basic example for chat-response

--- bot.py
+++ bot.py
@@ -16,6 +16,12 @@ class Maia(discord.Client):
         print(client.user.id)
         print('------')
 
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
 
 client = Maia()
 client.run(Token)
