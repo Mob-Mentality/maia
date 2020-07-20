d78d0ee4a1b2be6cc21e8949aabb1c5746f0bb39
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-19 20:04:18 -0400

    Added Hydrogen-1 to inventory

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ handler = logging.FileHandler(filename='maia.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+varMessageLength = 0
+varH1 = 0
+
 class Maia(discord.Client):
 
     async def on_ready(self):
@@ -23,15 +26,23 @@ class Maia(discord.Client):
         
         # Responds to the message "!test" with data about the message
         if message.content.startswith('!test'):
+            
+            varMessageLength = len(message.content)
+            varH1 = varH1 + varMessageLength
+            
             await message.channel.send('Message tts: {0.tts}'.format(message))
             await message.channel.send('Message type: {0.type}'.format(message))
             await message.channel.send('Message author: {0.author}'.format(message))
             await message.channel.send('Message content: {0.content}'.format(message))
-            await message.channel.send('Message length: {len(0.content)}'.format(message))
+            await message.channel.send('Message length: {}'.format(varMessageLength))
             await message.channel.send('Message nonce: {0.nonce}'.format(message))
             await message.channel.send('Message embeds: {0.embeds}'.format(message))
             await message.channel.send('Message channel: {0.channel}'.format(message))
             await message.channel.send('Message mention_everyone: {0.mention_everyone}'.format(message))
+            
+        if message.content.startswith('!inventory'):
+            
+            await message.channel.send('H1: {}'.format(varH1)
 
 client = Maia()
 client.run(Token)
