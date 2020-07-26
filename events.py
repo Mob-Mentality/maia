84a3d659e12c90bf22a18cf5d9b504e21982600e
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-25 21:10:36 -0400

    Created basic structure and added placeholders

---
+++ events.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def current_events(ops):
+
+  await message.channel.send('Events Message')
+  
+  # Daily Events
+  
+  # Limited Time Research
+  # Research
+  # Limited Time Ship
+  # Officer Recruit
+  # Limited Time Officer XP
+ 
