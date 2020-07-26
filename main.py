b51f2804d48b0ed0728232fd412a9a7be6a815b3
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-25 23:06:57 -0400

    Added Ansatz testing Code

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ handler = logging.FileHandler(filename='maia.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-varMessageLength = 0
-varH1 = 0
+# Initializing variables
+varMessageLength = 0 # Used to store message length for later use.
+varH1 = 5700 # Ansatz - Proton inventory
+varH2 = 0 # Ansatz - Deuterium inventory
+varAnsatzDelta = 0 # Ansatz - Used to store Ansatz material consumed
 
 class Maia(discord.Client):
 
+    # globalize variables
+    global varMessageLength
+    global varH1
+    global varH2
+    global varAnsatzDelta
+    
     async def on_ready(self):
         print('Logged in as')
         print(client.user.name)
@@ -26,11 +35,25 @@ class Maia(discord.Client):
         if message.author.id == self.user.id:
             return
         
+        #Ansatz Processing
+        varMessageLength = len(message.content)
+        varAnsatzDelta = varH1/20000
+        
+        if varAnsatzDelta < 1
+            varAnsatzDelta = 0
+        
+        varH1 = varH1 + varMessageLength - 2*varAnsatzDelta
+        varH2 = varH2 + varAnsatzDelta
+                    
+        if message.content.startswith('!inventory'):
+            
+            print('H1: {}.format(varH1))
+            print('H2: {}.format(varH2))
+            await message.channel.send('H1: {}'.format(varH1))
+            await message.channel.send('H2: {}'.format(varH2))
+        
         # Responds to the message "!test" with data about the message
         if message.content.startswith('!test'):
-            
-            varMessageLength = len(message.content)
-            varH1 = varH1 + varMessageLength
             
             await message.channel.send('Message tts: {0.tts}'.format(message))
             await message.channel.send('Message type: {0.type}'.format(message))
@@ -41,11 +64,6 @@ class Maia(discord.Client):
             await message.channel.send('Message embeds: {0.embeds}'.format(message))
             await message.channel.send('Message channel: {0.channel}'.format(message))
             await message.channel.send('Message mention_everyone: {0.mention_everyone}'.format(message))
-            
-        if message.content.startswith('!inventory'):
-            
-            print(varH1)
-            await message.channel.send('H1: {}'.format(varH1))
 
         # Events commands
         if message.content.startswith('!events'):
